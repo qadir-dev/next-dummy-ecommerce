bbc71780a4467bfd32ee59b6045b2538e43c779b
Qadir <qadir.qanbarpoor@gmail.com>
2025-04-06 22:44:50 -0700

    0.0.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Button from "./components/button";
-
 
 const Home = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-center">Dummy Coffee</h1>
-      <Button variant="alert">Click</Button>
+      
     </>
   );
 };
