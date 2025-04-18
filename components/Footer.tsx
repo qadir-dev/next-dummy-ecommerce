ce9fccde43493cc5e333e5dd9767c706ec219d9c
Qadir <qadir.qanbarpoor@gmail.com>
2025-04-18 01:10:41 -0700

    duration

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,13 @@ const Footer = () => {
     <footer className="bg-gray-100 text-gray-700 py-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-around items-center">
         <div className="flex space-x-6 mb-4 md:mb-0">
-          <Link href="/privacy" className="text-[#7b6e5d] hover:text-[#6f4e37]">
+          <Link href="/privacy" className="text-[#7b6e5d] hover:text-[#6f4e37] transition duration-300">
             Privacy
           </Link>
-          <Link href="/terms" className="text-[#7b6e5d] hover:text-[#6f4e37]">
+          <Link href="/terms" className="text-[#7b6e5d] hover:text-[#6f4e37] transition duration-300">
             Terms
           </Link>
-          <Link href="/contact" className="text-[#7b6e5d] hover:text-[#6f4e37]">
+          <Link href="/contact" className="text-[#7b6e5d] hover:text-[#6f4e37] transition duration-300">
             Contact
           </Link>
         </div>
